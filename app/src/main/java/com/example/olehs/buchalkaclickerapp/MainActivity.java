b20d50e1c8bae3844f378e9b4fba0c6e189e7ffd
Oleh Stelmakh <olehs@Olehs-MacBook-Pro.local>
2018-07-16 22:13:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/olehs/buchalkaclickerapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.olehs.buchalkaclickerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+    }
+}
